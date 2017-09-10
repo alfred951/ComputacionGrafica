6a1990c854799501e14b82e17de4698ac0e12fde
Alfred951 <lutisluis@gmail.com>
2017-09-10 15:59:32 -0500

    Reto 5 carro

---
+++ Tareas/Reto 5 - Transformaciones Perspectiva/Objeto3DCamaraArbitraria/src/Math/Uvn.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Math;
+
+/**
+ *
+ * @author htrefftz
+ */
+public class Uvn extends Matrix4x4  {
+    Vector4 from;
+    Vector4 lookAt;
+    Vector4 up;
+
+    public Uvn() {
+        super();
+    }
+    
+    public Uvn(Vector4 from, Vector4 lookAt, Vector4 up) {
+        super();
+    }
+    
+    
+}
